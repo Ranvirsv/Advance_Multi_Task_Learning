928307790d81496aae618ed8b5ad65805bf24ef9
ranivrsv <ranvirsv@gmail.com>
2026-03-06 14:26:29 +0530

    feat: Initialize MMoE model structure and set up data loading, embedding, and model instantiation in the main execution script.

---
+++ main.py
@@ -0,0 +1,28 @@
+from Dataset import load_dataset, ToxicityDataset
+from MMoE import MMoE
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+
+load_dotenv()
+
+## File to get the data, and run models 
+
+def main():
+    toxicity_train_path = os.getenv("TOXICITY_TRAIN_PATH")
+    toxicity_train_df = load_dataset(toxicity_train_path)
+    
+    embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+    
+    toxicity_train_dataset = ToxicityDataset(toxicity_train_df, embedding_model)
+    toxicity_train_dataloader = DataLoader(toxicity_train_dataset, batch_size=32, shuffle=True)
+
+    toxicity_test_path = os.getenv("TOXICITY_TEST_PATH")
+    toxicity_test_df = load_dataset(toxicity_test_path)
+    
+    toxicity_test_dataset = ToxicityDataset(toxicity_test_df, embedding_model)
+    toxicity_test_dataloader = DataLoader(toxicity_test_dataset, batch_size=32, shuffle=True)
+
+    mmoe_model = MMoE()
+    
+if __name__ == "__main__":
+    main()
